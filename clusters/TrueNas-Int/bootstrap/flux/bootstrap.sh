625a5aaa9d4ecc7368efb737ee13d042057a35c7
Steve Tomaszewski <nomsplease.code@gmail.com>
2024-08-07 15:34:58 -0400

    Remove the clusters folder

--- clusters/TrueNas-Int/bootstrap/flux/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl apply -f namespace.yaml
-sops -d age-key.secret.sops.yaml | kubectl apply -f -
-sops -d github-deploy-key.secret.sops.yaml | kubectl apply -f -
-kubectl apply -f https://github.com/fluxcd/flux2/releases/latest/download/install.yaml
-kubectl apply -k ../../resources/
